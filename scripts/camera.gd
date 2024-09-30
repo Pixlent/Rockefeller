b69f6e193a5aa740e4805fd9fb9c3f1167ee9a7d
Pixlent <2beheima@gmail.com>
2024-09-30 03:22:15 +0200

    Cleaned up movement code
    Cleaned up movement and zoom code, as well as started a basic UI implementation

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -8,13 +8,25 @@ const ZOOM_IN_SPEED := 5
 
 var new_zoom := zoom.x
 var new_position := position
+var hovering_ui: bool
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _on_scroll_container_mouse_entered():
+	hovering_ui = true
 
-func _input(event):
-	# Zoom logic
+func _on_scroll_container_mouse_exited():
+	hovering_ui = false
+
+func _input(event: InputEvent):
+	_handle_focus(event)
+	_handle_zoom(event)
+	_handle_movement(event)
+
+func _handle_focus(event: InputEvent):
+	if event is InputEventMouseButton:
+		if event.double_click:
+			_focus(get_global_mouse_position())
+
+func _handle_zoom(event: InputEvent):
 	if event is InputEventMouseButton:
 		if event.is_released():
 			return
@@ -24,22 +36,21 @@ func _input(event):
 		if input_zoom == -1 and new_zoom >= ZOOM_UPPER_BOUND: input_zoom = 0
 		if input_zoom == 1 and new_zoom <= ZOOM_LOWER_BOUND: input_zoom = 0
 		
-		new_zoom -= input_zoom
-		
-	# Movement logic
+		new_zoom -= (input_zoom * 0.2) * new_zoom
+
+func _handle_movement(event: InputEvent):
 	if event is InputEventMouseMotion:
 		if event.button_mask == MOUSE_BUTTON_MASK_LEFT:
 			new_position -= event.relative / zoom
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if new_zoom > zoom.x:
 		zoom.x = lerp(zoom.x, new_zoom, ZOOM_IN_SPEED*delta)
 	else:
 		zoom.x = lerp(zoom.x, new_zoom, ZOOM_OUT_SPEED*delta)
-
-	zoom.y = zoom.x
 	
+	zoom.y = zoom.x
 	position = new_position
 
-	pass
+func _focus(_target_position: Vector2):
+	new_position = _target_position
